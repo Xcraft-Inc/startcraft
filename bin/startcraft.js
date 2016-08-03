43d6f2391a59e547dca3e0736bde1566e5e77492
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-08-03 14:03:08 +0200

    Handle the post and preinstall lifecycle event from npm
    - preinstall removes the user module symlinks in order to prevent
      warnings with npm.
    - postinstall doesn't create anymore symlinks on node_modules in
      the user module directories. Note that it breaks the use of the
      bugged require-self module.

--- bin/startcraft.js
+++ bin/startcraft.js
@@ -1,127 +1,24 @@
 #!/usr/bin/env node
 'use strict';
 
-const fs     = require ('fs');
-const path   = require ('path');
-const watt   = require ('watt');
-const shrew  = require ('shrew');
-const merge  = require ('merge-deep');
-const spawn  = require ('child_process').spawn;
+const lifecycleEnv   = 'npm_lifecycle_event';
+const lifecycleEvent = process.env[lifecycleEnv];
 
-const root = shrew ();
+let lcEvent = null;
 
-function loadConfig () {
-  return JSON.parse (fs.readFileSync (path.join (root, '.scrc')));
-}
-
-const config = loadConfig ();
-
-const npm = (verb, modPath, cwd, next) => {
-  console.log (`npm ${verb} ${modPath}`);
-
-  let args = [verb];
-  if (Array.isArray (modPath)) {
-    args = args.concat (modPath);
-  } else {
-    args.push (modPath);
+switch (lifecycleEvent) {
+  case 'preinstall':
+  case 'postinstall': {
+    lcEvent = require (`../lib/events/${lifecycleEvent}.js`);
+    break;
   }
-  args.concat (config.npmargs);
-
-  const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
-  const res = spawn (npm, args, {
-    stdio: ['ignore', 1, 2],
-    cwd: cwd || __dirname
-  });
-  res.on ('close', next);
-};
-
-function parsePackage (pkgPath) {
-  let def = fs.readFileSync (pkgPath);
-  def = JSON.parse (def);
-
-  const list = {};
-  const packages = config.modules.map (mod => path.basename (mod));
-
-  [def.dependencies, def.devDependencies]
-    .filter (deps => !!deps)
-    .forEach (deps => {
-      Object
-        .keys (deps)
-        .filter (pkg => {
-          return packages.indexOf (pkg) === -1;
-        })
-        .forEach (pkg => {
-          if (!list.hasOwnProperty (pkg)) {
-            list[pkg] = [];
-          }
-          list[pkg].push (deps[pkg]);
-        });
-    });
-
-  return list;
-}
-
-function symlink (src, dst) {
-  try {
-    fs.symlinkSync (src, dst, 'junction');
-  } catch (ex) {
-    if (ex.code !== 'EEXIST') {
-      throw ex;
-    }
+  default: {
+    console.error (`lcEvent ${lifecycleEvent} not supported`);
+    process.exit (1);
   }
 }
 
-const boot = watt (function * (next) {
-  let mustThrown = false;
-  let list = {};
-
-  config.modules.forEach (pkgPath => {
-    const pkgJsonPath = path.join (root, pkgPath, 'package.json');
-    list = merge (list, parsePackage (pkgJsonPath));
-  });
-
-  Object
-    .keys (list)
-    .forEach (mod => {
-      if (list[mod].length > 1) {
-        console.error (`Clash with ${mod} versions ${list[mod].join (', ')}`);
-        mustThrown = true;
-      }
-    });
-
-  if (mustThrown) {
-    throw new Error ('clash');
-  }
-
-  config.modules.forEach (pkgPath => {
-    const mod = path.join (root, 'node_modules', path.basename (pkgPath));
-    try {
-      const st = fs.lstatSync (mod);
-      if (st.isSymbolicLink ()) {
-        fs.unlinkSync (mod);
-      }
-    } catch (ex) {
-      if (ex.code !== 'ENOENT') {
-        throw ex;
-      }
-    }
-  });
-
-  yield npm ('install', Object.keys (list).map (mod => `${mod}@${list[mod][0]}`), root, next);
-
-  config.modules.forEach (pkgPath => {
-    symlink (
-      path.join (root, 'node_modules'),
-      path.join (root, pkgPath, 'node_modules')
-    );
-    symlink (
-      path.join (root, pkgPath),
-      path.join (root, 'node_modules', path.basename (pkgPath))
-    );
-  });
-});
-
-boot ((err) => {
+lcEvent ((err) => {
   if (err) {
     console.error (err);
   } else {
