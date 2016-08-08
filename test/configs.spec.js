155472f66e504bad4248bad75c64f0bbf9885f49
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-08-08 23:14:23 +0200

    Fix the test accordingly to the previous commit

--- test/configs.spec.js
+++ test/configs.spec.js
@@ -7,8 +7,15 @@ const npmEnv = 'npm_package_name';
 process.env[npmEnv] = 'startcraft';
 
 describe ('check config', function () {
-  it ('#config', function () {
-    const config = require ('../lib/config.js');
-    console.dir (config.modules);
+  it ('#modules', function () {
+    const {modules} = require ('../lib/config.js');
+    expect (modules[0]).to.be.eql ({
+      def: {},
+      src: null,
+      dst: null
+    });
+    expect (modules[1].def.name).to.be.eql ('test-module');
+    expect (modules[1].src).to.match (/.*test[/\\]test-module/);
+    expect (modules[1].dst).to.match (/.*node_modules[/\\]test-module/);
   });
 });
