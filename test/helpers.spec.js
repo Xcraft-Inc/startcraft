b9bdd037f930a480cf2426d4c879272350acaf9c
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-08-09 09:22:04 +0200

    Add a semver helper to compare ranges for max supported version

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -30,4 +30,25 @@ describe ('check helpers', function () {
     expect (helpers.isOsCompatible (def5, 'darwin')).to.be.false;
     expect (helpers.isOsCompatible (def5, 'linux')).to.be.true;
   });
+
+  it ('#isSemverSatisfies', function () {
+    const ranges = [
+      [true,  ['^1.0.0',           '^1.9.2']],                        // max: x.x.x, x.x.x
+      [false, ['^1.5.0',           '^2.1.2']],                        // max: 1.x.x, 2.x.x
+      [false, ['~1.5.0',           '~1.6.0']],                        // max: 1.5.x, 1.6.x
+      [true,  ['~1.5.0',           '<1.6']],                          // max: 1.5.x, 1.5.x
+      [false, ['1.1.0',            '^1.1.0']],                        // max: 1.1.0, 1.x.x
+      [true,  ['1.0.0 - 2.0.0',    '>1.1.0 <=2.0.0']],                // max: 2.0.0, 2.0.0
+      [true,  ['2.0.0',            '<=2.0.0']],                       // max: 2.0.0, 2.0.0
+      [false, ['2.0.0',            '<2.0.0']],                        // max: 2.0.0, 1.x.x
+      [true,  ['*',                '>5']],                            // max: x.x.x, x.x.x
+      [false, ['*',                '<5']],                            // max: x.x.x, 4.x.x
+      [true,  ['>1.1.1',           '>1.1.2']],                        // max: x.x.x, x.x.x
+      [true,  ['^1.1.1',           '^1.9.2',           '>1.0.0 <2']], // max: 1.x.x, 1.x.x, 1.x.x
+      [true,  ['3.1.1 || <=2.1.1', '2.1.1']],                         // max: (3.1.1 || 2.1.1), 2.1.1
+      [true,  ['3.1.1 || <=2.1.1', '>4 || 2.1.1']]                    // max: (3.1.1 || 2.1.1), (x.x.x || 2.1.1)
+    ];
+
+    ranges.forEach (range => expect (helpers.isSemverSatisfies (range[1])).to.be.eql (range[0]));
+  });
 });
